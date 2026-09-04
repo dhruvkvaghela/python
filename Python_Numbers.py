e8d74986b320759a1680175a85144ef4cb2af4a6
dhruvkvaghela <dhruv.kvagh@gmail.com>
2026-09-04 11:03:52 -0400

    Add files via upload

---
+++ Python_Numbers.py
@@ -0,0 +1,59 @@
+# Python Numbers
+
+# Integer
+a = 10
+
+# Float
+b = 5.5
+
+print(a)
+print(b)
+
+# Basic calculations
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+# Other number types
+print(10 // 3)  # Floor division
+print(10 % 3)   # Modulus
+print(2 ** 3)   # Exponentiation
+
+
+# Python Strings
+
+name = "John"
+message = "Welcome to Python!"
+
+print(name)
+print(message)
+
+# String concatenation
+first_name = "John"
+last_name = "Doe"
+
+full_name = first_name + " " + last_name
+print(full_name)
+
+# String repetition
+print("Python " * 3)
+
+# String length
+print(len(name))
+
+# Accessing characters
+word = "Python"
+
+print(word[0])
+print(word[1])
+print(word[-1])
+
+# String methods
+text = "hello world"
+
+print(text.upper())
+print(text.lower())
+print(text.capitalize())
+print(text.title())
+
